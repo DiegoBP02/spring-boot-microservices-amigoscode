9da3841f7751f63a0c8bedb3710860078ff9a285
Diego Batista Pereira <diegobpcelular@gmail.com>
2023-09-26 17:42:33 -0300

    feat: first commit with fraud and customer services

---
+++ customer/src/main/java/com/programming/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.programming.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
---
+++ fraud/src/main/java/com/programming/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.programming.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
